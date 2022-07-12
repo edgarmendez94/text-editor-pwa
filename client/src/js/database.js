761c87645e3cd6022928e0a839c17f9efdc73382
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-12 18:57:02 -0400

    fixed images going to wrong destination after building webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,14 +18,14 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, editor: content });
+  const request = store.put({ db: content });
   const result = await request;
   console.log('result.value', result);
   return result;
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getAllDb = async () => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
